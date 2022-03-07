24287645063241c1f5c70d50f2fe7e9cd4a311a8
trinhduyen <trinhdk5b1@gmail.com>
2022-03-07 16:41:11 +0700

    Update settings.py

--- DoiXe/settings.py
+++ DoiXe/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles' )
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
